d034901c3c6b67779d15c629375ce230059bfcd9
garza <danni900g@gmail.com>
2018-05-30 22:38:52 -0600

    Se crea la clase Graph y el metodo floyd
    Ahora con la clase graph contamos con el metodo floyd, ahora se puede
    calcular la ruta mas corta entre todos los nodos siempre y cuando se
    introduzca una matriz de adyacencia en el parametro del metodo, en vez
    de infinito como dice la teoria utilizamos el numero 999999999

---
+++ src/GraphsLogic/Graph.java
@@ -0,0 +1,90 @@
+
+package GraphsLogic;
+
+/**
+ *
+ * @author dgarcia
+ */
+public class Graph{
+  public Graph(){
+
+  }
+
+  public String Floyd(long [][] matrizAd){
+    int vert = matrizAd.length;
+    long [][] matADyacencia = matrizAd;
+    String [][] path  = new String [vert][vert];
+    String [][] auxPath = new String [vert][vert];
+    String roadTravel = "", cad = "",paths ="";
+    int i,j,k;
+    float temp1,temp2,temp3,temp4,min;
+    //Inicializando matriz de caminos y caminos auxiliares
+
+    for(i = 0;i<vert;i++){
+      for(j=0;j<vert;j++){
+        path[i][j] = "";
+        auxPath[i][j] = "";
+      }
+    }
+    for(k = 0; k<vert ; k++){
+      for(i = 0; i<vert ; i++){
+        for(j = 0 ; j<vert ; j++){
+          temp1 = matADyacencia[i][j];
+          temp2 = matADyacencia[i][k];
+          temp3 = matADyacencia[k][j];
+          temp4 = temp2+temp3;
+          //encotrando minimo
+          min = Math.min(temp1,temp4);
+          if(temp1 != temp4){
+            if(min == temp4){
+              roadTravel ="";
+              auxPath[i][j] = k+"";
+              path[i][j] = this.pathsR(i ,k , auxPath,roadTravel)+(k+1);
+            }
+          }
+          matADyacencia[i][j] = (long)min;
+        }
+      }
+    }
+
+    for(i = 0 ;i <vert ; i++){
+      for(j = 0 ; j<vert ; j++){
+        cad = cad + "["+matADyacencia[i][j]+"]";
+      }
+      cad = cad+"\n";
+    }
+    //////////////////////
+    for(i = 0 ; i<vert ; i++){
+      for(j = 0 ; j<vert; j++){
+         if(matADyacencia[i][j] != 1000000000){
+           if(i != j){
+             if(path[i][j].equals("")){
+               paths+="De ("+(i+1)+"---->"+(j+1)+") Ir por..("+(i+1)+", "+(j+1)+")\n";
+
+
+             }else{
+               paths+="De ("+(i+1)+"---->"+(j+1)+") Ir por..("+(i+1)+", "+path[i][j]+", "+(j+1)+")\n";
+
+             }
+           }
+         }
+      }
+    }
+    return "Matriz de caminos cortos: \n"+cad+
+    "\nDiferentes caminos:"+paths;
+
+  }
+  public String pathsR(int i , int k , String[][] auxP , String roadT){
+    if(auxP[i][k].equals("")){
+      return "";
+    }else{
+      //Recursividad
+      roadT += pathsR(i,Integer.parseInt(auxP[i][k]),auxP,roadT)+(Integer.parseInt(auxP[i][k])+1)+", ";
+      return roadT;
+    }
+  }
+
+  public static void main(String args[]){
+
+  }
+}
