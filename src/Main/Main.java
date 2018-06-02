fbb2922ffee59257577eb3b72c2da9d54acbaeb1
garza <danni900g@gmail.com>
2018-06-02 03:41:49 -0600

    Misma mier

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,7 +5,6 @@
  */
 package Main;
 
-import Structures_Logic.Graph;
 import Ventanas.Gestor;
 
 /**
@@ -18,13 +17,11 @@ public class Main
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Gestor gestor= new Gestor();
         gestor.showMain();
 
-
-          
-          
     }
     
 }
